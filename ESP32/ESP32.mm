c68d6d8adba261843fe0e4774a6c5f22d5252c9c
linblink <827650791@qq.com>
2026-05-16 14:55:13 +0800

    update

---
+++ ESP32/ESP32.mm
@@ -0,0 +1,247 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="ESP32" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1778768643365" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_FLOW"><hook NAME="MapStyle" background="#2e3440ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" followedTemplateLocation="template:/dark_nord_template.mm" followedMapLastTime="1771844082000" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="概念" POSITION="top_or_left" ID="ID_1073860858" CREATED="1778770355991" MODIFIED="1778770371690">
+<font BOLD="true"/>
+<node TEXT="FreeRTOS" ID="ID_98921381" CREATED="1778768643377" MODIFIED="1778768658421">
+<node TEXT="RTOS" ID="ID_1270068886" CREATED="1778768658597" MODIFIED="1778768662963">
+<node TEXT="RealTime Operating System" ID="ID_651586924" CREATED="1778768663067" MODIFIED="1778768670907"/>
+</node>
+<node TEXT="Components" ID="ID_193213110" CREATED="1778768774101" MODIFIED="1778768777177">
+<node TEXT="Task" ID="ID_298989342" CREATED="1778768778024" MODIFIED="1778768779750"/>
+<node TEXT="Schedular" ID="ID_1939176648" CREATED="1778768779980" MODIFIED="1778768786250"/>
+<node TEXT="Priority" ID="ID_882138465" CREATED="1778768786777" MODIFIED="1778768797705"/>
+<node TEXT="Delay" ID="ID_925501132" CREATED="1778768798086" MODIFIED="1778768801771"/>
+<node TEXT="Queue" ID="ID_1964165066" CREATED="1778768804740" MODIFIED="1778768806013"/>
+<node TEXT="Semaphore" ID="ID_1382324612" CREATED="1778768896473" MODIFIED="1778768898764"/>
+<node TEXT="Mutex" ID="ID_296058175" CREATED="1778768900706" MODIFIED="1778768905559"/>
+<node TEXT="Tick" ID="ID_712578198" CREATED="1778768910044" MODIFIED="1778768911074"/>
+</node>
+<node TEXT="#include" ID="ID_516952639" CREATED="1778769105115" MODIFIED="1778769111520">
+<node TEXT="freertos/FreeRTOS.h" ID="ID_593107964" CREATED="1778769111623" MODIFIED="1778769120608"/>
+<node TEXT="freertos/task.h" ID="ID_1719054044" CREATED="1778769111623" MODIFIED="1778769133700"/>
+</node>
+</node>
+<node TEXT="LVGL" ID="ID_1440956362" CREATED="1778768950219" MODIFIED="1778768951150">
+<node TEXT="Light and Versatile Graphics Library" ID="ID_1399966778" CREATED="1778768952494" MODIFIED="1778769018675"/>
+</node>
+<node TEXT="NVS" ID="ID_71822781" CREATED="1778769179424" MODIFIED="1778769181402">
+<node TEXT="Non-Volatile Storage" ID="ID_1141140302" CREATED="1778769181669" MODIFIED="1778769188121">
+<node ID="ID_1025635094" CREATED="1778769195655" MODIFIED="1778769195655"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    #include &quot;nvs_flash.h&quot;
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="LED" ID="ID_379089050" CREATED="1778769208274" MODIFIED="1778769209349">
+<node TEXT="#include &quot;led.h&quot;" ID="ID_1431940814" CREATED="1778769212463" MODIFIED="1778769212463"/>
+</node>
+<node TEXT="SPI" ID="ID_957482774" CREATED="1778769228453" MODIFIED="1778769231042">
+<node TEXT="Serial Peripheral Interface" ID="ID_1062809940" CREATED="1778769261628" MODIFIED="1778769270398"/>
+<node TEXT="#include &quot;spi.h&quot;" ID="ID_1908444399" CREATED="1778769256990" MODIFIED="1778769256990"/>
+<node TEXT="SDCARD" ID="ID_1863357033" CREATED="1778769686037" MODIFIED="1778769689291">
+<node TEXT="#include &quot;spi_sdcard.h&quot;" ID="ID_486515042" CREATED="1778769696062" MODIFIED="1778769696062"/>
+</node>
+</node>
+<node TEXT="IIC(I2C)" ID="ID_1218199985" CREATED="1778769381923" MODIFIED="1778769385157">
+<node TEXT="Inter-Integrated Circult" ID="ID_574167644" CREATED="1778769405848" MODIFIED="1778769413954"/>
+<node TEXT="低速、两线制、串行" ID="ID_862919417" CREATED="1778769414647" MODIFIED="1778769426410"/>
+<node ID="ID_1230851577" CREATED="1778769448545" MODIFIED="1778769448545"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div class="relative w-full mt-4 mb-1">
+      <div class="">
+        <div class="relative">
+          <div class="h-full min-h-0 min-w-0">
+            <div class="h-full min-h-0 min-w-0">
+              <div class="border border-token-border-light border-radius-3xl corner-superellipse/1.1 rounded-3xl">
+                <div class="h-full w-full border-radius-3xl bg-token-bg-elevated-secondary corner-superellipse/1.1 overflow-clip rounded-3xl lxnfua_clipPathFallback">
+                  <div class="relative">
+                    <div class="">
+                      <div class="relative z-0 flex max-w-full">
+                        <div id="code-block-viewer" dir="ltr" class="q9tKkq_viewer cm-editor z-10 light:cm-light dark:cm-light flex h-full w-full flex-col items-stretch ͼs ͼ16">
+                          <div class="cm-scroller">
+                            <pre class="cm-content q9tKkq_readonly m-0"><code><span class="ͼu">#include &quot;iic.h&quot;</span></code></pre>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="LCD" ID="ID_1573912913" CREATED="1778769454184" MODIFIED="1778769456194">
+<node ID="ID_356901806" CREATED="1778769458539" MODIFIED="1778769458539"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div class="relative w-full mt-4 mb-1">
+      <div class="">
+        <div class="relative">
+          <div class="h-full min-h-0 min-w-0">
+            <div class="h-full min-h-0 min-w-0">
+              <div class="border border-token-border-light border-radius-3xl corner-superellipse/1.1 rounded-3xl">
+                <div class="h-full w-full border-radius-3xl bg-token-bg-elevated-secondary corner-superellipse/1.1 overflow-clip rounded-3xl lxnfua_clipPathFallback">
+                  <div class="relative">
+                    <div class="">
+                      <div class="relative z-0 flex max-w-full">
+                        <div id="code-block-viewer" dir="ltr" class="q9tKkq_viewer cm-editor z-10 light:cm-light dark:cm-light flex h-full w-full flex-col items-stretch ͼs ͼ16">
+                          <div class="cm-scroller">
+                            <pre class="cm-content q9tKkq_readonly m-0"><code><span class="ͼu">#include &quot;iic.h&quot;</span></code></pre>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="XL9555" ID="ID_720605745" CREATED="1778769473936" MODIFIED="1778769483489">
+<node TEXT="#include &quot;xl9555.h&quot;" ID="ID_17723935" CREATED="1778769483913" MODIFIED="1778769483913"/>
+<node TEXT="GPIO" ID="ID_384641161" CREATED="1778769497519" MODIFIED="1778769501058"/>
+</node>
+<node TEXT="USB" ID="ID_616500210" CREATED="1778769566158" MODIFIED="1778769568033">
+<node TEXT="TinyUSB" ID="ID_1921690651" CREATED="1778769568134" MODIFIED="1778769570385">
+<node TEXT="#include &quot;tud_sd.h&quot;" ID="ID_559544749" CREATED="1778769579435" MODIFIED="1778769579435"/>
+</node>
+</node>
+<node TEXT="SD_CARD" ID="ID_1743477813" CREATED="1778769709562" MODIFIED="1778769712954">
+<node TEXT="SD卡的读写模式" ID="ID_1234141381" CREATED="1778769713224" MODIFIED="1778769717191">
+<node TEXT="SDIO" ID="ID_1954524166" CREATED="1778769717811" MODIFIED="1778769719698"/>
+<node TEXT="SPI" ID="ID_1268613901" CREATED="1778769720491" MODIFIED="1778769721801"/>
+</node>
+</node>
+<node TEXT="EEPROM" ID="ID_1843598172" CREATED="1778769871095" MODIFIED="1778769872977">
+<node TEXT="Electrically Erasable Programmable Read-Only Memory" ID="ID_1136437689" CREATED="1778769879302" MODIFIED="1778769879302"/>
+</node>
+</node>
+<node TEXT="工作流程" POSITION="top_or_left" ID="ID_1289673196" CREATED="1778770377326" MODIFIED="1778770381404">
+<node TEXT="启动流程" ID="ID_351598757" CREATED="1778770381573" MODIFIED="1778770386359">
+<node TEXT="ROM Bootloader" ID="ID_939796682" CREATED="1778770386513" MODIFIED="1778770396853"/>
+<node TEXT="二级Bootloader" ID="ID_1042399180" CREATED="1778770397005" MODIFIED="1778770406678">
+<node TEXT="初始化 flash" ID="ID_1036202012" CREATED="1778770541310" MODIFIED="1778770545052"/>
+</node>
+<node TEXT="FreeRTOS启动" ID="ID_1940467090" CREATED="1778770406798" MODIFIED="1778770411622"/>
+<node TEXT="app_mian()" ID="ID_147495237" CREATED="1778770411799" MODIFIED="1778770600920">
+<font BOLD="true"/>
+<node TEXT="是一个 FreeRTOS Task" ID="ID_1039446518" CREATED="1778770601607" MODIFIED="1778770612535"/>
+</node>
+</node>
+</node>
+</node>
+</map>
