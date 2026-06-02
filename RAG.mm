1d6c2ae55eba96b8ac7660a4a1aef94a56977e7e
linblink <827650791@qq.com>
2026-06-02 19:29:48 +0800

    update

---
+++ RAG.mm
@@ -0,0 +1,137 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="RAG" FOLDED="false" ID="ID_1949060118" CREATED="1780283851261" MODIFIED="1780283895567">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#2e3440ff" zoom="0.26333117">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" show_tags="UNDER_NODES" showTagCategories="false" associatedTemplateLocation="file:/C:/Users/wangb/Desktop/my_study_notes/软考高级系统架构师/软考高级系统架构师.mm"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font NAME="Microsoft YaHei" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<font NAME="Microsoft YaHei"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Microsoft YaHei" SIZE="11" BOLD="false" UNDERLINED="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+<font NAME="Microsoft YaHei"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<font NAME="Microsoft YaHei"/>
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac">
+<font NAME="Microsoft YaHei"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<font NAME="Microsoft YaHei"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font NAME="Microsoft YaHei" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm">
+<font NAME="Microsoft YaHei"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<font NAME="Microsoft YaHei"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Microsoft YaHei" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#ff6699" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" TEXT_ALIGN="CENTER">
+<font NAME="Microsoft YaHei" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font NAME="Microsoft YaHei" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font NAME="Microsoft YaHei" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font NAME="Microsoft YaHei" SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font NAME="Microsoft YaHei" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font NAME="Microsoft YaHei" SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font NAME="Microsoft YaHei" SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="涉及内容" POSITION="bottom_or_right" ID="ID_85284523" CREATED="1780283922601" MODIFIED="1780283973564">
+<node TEXT="向量数据库" ID="ID_868455314" CREATED="1780283973783" MODIFIED="1780283976760"/>
+<node TEXT="数据加载" ID="ID_1006037312" CREATED="1780283976892" MODIFIED="1780283980202"/>
+<node TEXT="数据切片" ID="ID_93694262" CREATED="1780283980343" MODIFIED="1780283982959"/>
+<node TEXT="嵌入模型" ID="ID_253617446" CREATED="1780283983106" MODIFIED="1780283986219">
+<node TEXT="openai" ID="ID_120753333" CREATED="1780284161845" MODIFIED="1780284164859"/>
+<node TEXT="bge-large" ID="ID_572459343" CREATED="1780284164983" MODIFIED="1780284167321">
+<node TEXT="MIT开源协议" ID="ID_558849861" CREATED="1780284176638" MODIFIED="1780284179249"/>
+</node>
+</node>
+<node TEXT="检索器" ID="ID_1823134004" CREATED="1780283986369" MODIFIED="1780283991323"/>
+<node TEXT="Rerankers" ID="ID_864772784" CREATED="1780283991683" MODIFIED="1780283998034">
+<node TEXT="加权排序" ID="ID_648675158" CREATED="1780284310608" MODIFIED="1780284315365"/>
+<node TEXT="RRFRanker" ID="ID_768621868" CREATED="1780284315517" MODIFIED="1780284318548"/>
+</node>
+<node TEXT="Agent+RAG" ID="ID_554390999" CREATED="1780283998187" MODIFIED="1780284001283"/>
+<node TEXT="Graph+RAG" ID="ID_357375238" CREATED="1780284001432" MODIFIED="1780284004715"/>
+<node TEXT="多源RAG" ID="ID_891466821" CREATED="1780284004863" MODIFIED="1780284012177"/>
+<node TEXT="评估RAG" ID="ID_968137508" CREATED="1780284012337" MODIFIED="1780284014966"/>
+</node>
+<node TEXT="项目需求" POSITION="bottom_or_right" ID="ID_1425130356" CREATED="1780285832717" MODIFIED="1780285835493">
+<node TEXT="弱需求" ID="ID_1996760527" CREATED="1780285854120" MODIFIED="1780285858902"/>
+</node>
+<node POSITION="top_or_left" ID="ID_126171653" CREATED="1780284332478" MODIFIED="1780284332478"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <b data-path-to-node="0" data-index-in-node="9">Retrieval-Augmented Generation</b>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="向量数据库" POSITION="bottom_or_right" ID="ID_1642096042" CREATED="1780285487971" MODIFIED="1780285492343">
+<node TEXT="特化向量数据库" ID="ID_1512058566" CREATED="1780285803351" MODIFIED="1780285809014">
+<node TEXT="Milvus" POSITION="bottom_or_right" ID="ID_1188328825" CREATED="1780285492482" MODIFIED="1780285497055"/>
+</node>
+<node TEXT="支持向量检索的数据库" ID="ID_1327948264" CREATED="1780285809166" MODIFIED="1780285814301"/>
+</node>
+</node>
+</map>
